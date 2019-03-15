62dc8e70bcf29dd34129b27ebefa7ac51fc6b05b
Jon Warriner <jonpaul.warriner@colorado.edu>
2019-03-14 21:36:48 -0400

    added ring_t typedef, started implementing init, insert and remove

--- ring.c
+++ ring.c
@@ -2,13 +2,56 @@
 ** Circular buffer
 ** ECEN5813
 **========================================================================*/
+#include "ring.h"
+#include <stdlib.h>
 
-typedef struct
+ring_t *init( int32_t length )
 {
-	char *Buffer;
-	int Length;
-	int Ini;
-	int Outi;
-} ring_t;
+char *pbuf = 0;
+ring_t *r = 0;
 
+    // make sure the requested length makes sense
+    if(length <= 0)
+    {
+        return 0;
+    }
+
+    // create the buffer
+    pbuf = malloc(length);
+    if(pbuf)
+    {
+        r = (ring_t *)malloc(sizeof(ring_t));
+        return(r);
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int32_t insert( ring_t *ring, char data )
+{
+    if( ring->Ini - ring->Outi < ring->Length ) 
+    {
+        ring->Buffer[ring->Ini++ & ring->Length] = data;
+    }
+
+    return 0;
+}
+
+int32_t remove( ring_t *ring, char *data )
+{
+    if( ring->Outi != ring->Ini )
+    {
+        *data = ring->Buffer[ring->Outi++ & ring->Length];
+    }
+
+    return 0;
+}
+
+int32_t entries( ring_t *ring )
+{
+    return 0;
+}
 
--- ring.h
+++ ring.h
@@ -6,9 +6,19 @@
 #ifndef RING_H
 #define RING_H
 
-ring_t *init( int length );
-int insert( ring_t *ring, char data );
-int remove( ring_t *ring, char *data );
-int entries( ring_t *ring );
+#include <stdint.h>
+
+typedef struct
+{
+    char *Buffer;
+    int32_t Length;
+    int32_t Ini;
+    int32_t Outi;
+}ring_t;
+
+ring_t *init( int32_t length );
+int32_t insert( ring_t *ring, char data );
+int32_t remove( ring_t *ring, char *data );
+int32_t entries( ring_t *ring );
 
 #endif
